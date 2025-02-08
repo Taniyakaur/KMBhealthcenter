2f150211914dd3d31ee4138d133e24b54c9c8e52
fmagr <fede.magrograssi@gmail.com>
2025-02-08 11:58:48 +0100

    modifica header

--- CasaSalute/homepage/urls.py
+++ CasaSalute/homepage/urls.py
@@ -5,7 +5,7 @@ from . import views    # import views from the same directory
 urlpatterns = [
     path ('admin/', admin.site.urls),  
     path ('',views.index,name='home'),  # path for the home page
-    path ('',views.about,name='about'),
-    path ('',views.contact,name='contact'),
+    path ('about/',views.about,name='about'),
+    path ('contact/',views.contact,name='contact'),
    
 ]
