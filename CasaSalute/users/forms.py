b81ab543dac41569b9f50c28988bfcf3fc9f0efe
fmagr <fede.magrograssi@gmail.com>
2025-02-11 17:21:04 +0100

    login (non funziona)
    sembra che ci sia un problema con la memorizzazione della password

--- CasaSalute/users/forms.py
+++ CasaSalute/users/forms.py
@@ -5,18 +5,46 @@ class MedicoForm(forms.ModelForm):
     class Meta:
         model = Medico
         fields = ['username', 'password', 'nome', 'cognome', 'specializzazione', 'telefono', 'email']
+    
+    def save(self, commit=True):                           # override del metodo save
+        user = super().save(commit=False)                  # se commit=False non salva l'istanza user
+        user.set_password(self.cleaned_data['password'])   # prende password dell'utente
+        if commit:
+            user.save()
+        return user
 
 class InfermiereForm(forms.ModelForm):
     class Meta:
         model = Infermiere
         fields = ['username', 'password', 'nome', 'cognome', 'telefono', 'email']
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
 
 class PazienteForm(forms.ModelForm):
     class Meta:
         model = Paziente
         fields = ['codice_sanitario', 'username', 'password', 'nome', 'cognome', 'data_di_nascita', 'indirizzo', 'telefono', 'email']
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
 class SegreteriaForm(forms.ModelForm):
     class Meta:
         model = Segreteria
         fields = ['username', 'password', 'nome', 'cognome', 'telefono', 'email']
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
--- CasaSalute/users/views.py
+++ CasaSalute/users/views.py
@@ -1,68 +1,146 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password, check_password
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Medico, Infermiere, Paziente, Segreteria
 
 # Create your views here.
+
+def create_paziente(username, password, nome, cognome, codice_sanitario, data_di_nascita, indirizzo, telefono, email):
+    hashed_password = make_password(password)
+    paziente = Paziente(
+        username=username,
+        password=hashed_password,
+        nome=nome,
+        cognome=cognome,
+        codice_sanitario=codice_sanitario,
+        data_di_nascita=data_di_nascita,
+        indirizzo=indirizzo,
+        telefono=telefono,
+        email=email
+    )
+    paziente.save()
+
+def create_infermiere(username, password, nome, cognome, data_di_nascita, indirizzo, telefono, email, reparto, giorni_sala_prelievi, giorni_sala_medicazioni):
+    hashed_password = make_password(password)
+    infermiere = Infermiere(
+        username=username,
+        password=hashed_password,
+        nome=nome,
+        cognome=cognome,
+        data_di_nascita=data_di_nascita,
+        indirizzo=indirizzo,
+        telefono=telefono,
+        email=email,
+        reparto=reparto,
+        giorni_sala_prelievi=giorni_sala_prelievi,
+        giorni_sala_medicazioni=giorni_sala_medicazioni
+    )
+    infermiere.save()
+
+def create_medico(username, password, nome, cognome, data_di_nascita, telefono, email, specializzazione):
+    hashed_password = make_password(password)
+    medico = Medico(
+        username=username,
+        password=hashed_password,
+        nome=nome,
+        cognome=cognome,
+        data_di_nascita=data_di_nascita,
+        telefono=telefono,
+        email=email,
+        specializzazione=specializzazione
+    )
+    medico.save()
+
+def create_segreteria(username, password, nome, cognome, data_di_nascita, telefono, email):
+    hashed_password = make_password(password)
+    segreteria = Segreteria(
+        username=username,
+        password=hashed_password,
+        nome=nome,
+        cognome=cognome,
+        data_di_nascita=data_di_nascita,
+        telefono=telefono,
+        email=email
+    )
+    segreteria.save()
+
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
-    if request.user.is_medico:
-        return render(request, "user/medico.html")
-    elif request.user.is_infermiere:
-        return render(request, "user/infermiere.html")
-    elif request.user.is_paziente:
-        return render(request, "user/paziente.html")
+    if isinstance(request.user, Medico):
+        return render(request, "users/medico.html")
+    elif isinstance(request.user, Infermiere):
+        return render(request, "users/infermiere.html")
+    elif isinstance(request.user, Paziente):
+        return render(request, "users/paziente.html")
+    elif isinstance(request.user, Segreteria):
+        return render(request, "users/segreteria.html")
     else:
-        return render(request, "user/user.html")  #generico
+        return render(request, "users/users.html")  #generico
+
+def authenticate_user(username, password):
+    user_models = [Paziente, Infermiere, Medico, Segreteria]
+    for model in user_models:
+        try:
+            user = model.objects.get(username=username)
+            print(f"Found user: {user.username}")  # debug
+            print(f"Checking password for {username}: {password} against {user.password}")
+            if check_password(password, user.password):  # Check the hashed password
+                return user
+            else:
+                print("Password does not match") # debug
+        except model.DoesNotExist:
+            print("User does not exist") # debug
+            continue
+    return None
+
 
 def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        
+        user = authenticate_user(username, password)
+        if user:
             login(request, user)
             if isinstance(user, Medico):
-                return HttpResponseRedirect(reverse("medico"))  # reindirizza alla pagina corrispondente del tipo di utente
+                return HttpResponseRedirect(reverse("medico"))
             elif isinstance(user, Infermiere):
                 return HttpResponseRedirect(reverse("infermiere"))
             elif isinstance(user, Paziente):
                 return HttpResponseRedirect(reverse("paziente"))
             elif isinstance(user, Segreteria):
                 return HttpResponseRedirect(reverse("segreteria"))
-            else:
-                return HttpResponseRedirect(reverse("index"))  # generico
-        else:
-            return render(request, "users/login.html", {
-                "message": "Autenticazione fallita."
-            })
-    return render(request, "users/login.html")  # Handle GET request to render login page
+        return render(request, "users/login.html", {
+            "message": "Autenticazione fallita."
+        })
+    return render(request, "users/login.html")
 
 def logout_view(request):
     logout(request)
     return render(request, "users/login.html",{
         "message": "Logout effettuato."
-    }) # Redirect to login page after logout
+    })
 
 def medico_view(request):
     if not request.user.is_authenticated or not isinstance(request.user, Medico):
         return HttpResponseRedirect(reverse("login"))
-    return render(request, "user/medico.html")
+    return render(request, "users/medico.html")
     # se non è autenticato o non è del tipo corretto rimanda al login
 
 def infermiere_view(request):
     if not request.user.is_authenticated or not isinstance(request.user, Infermiere):
         return HttpResponseRedirect(reverse("login"))
-    return render(request, "user/infermiere.html")
+    return render(request, "users/infermiere.html")
 
 def paziente_view(request):
     if not request.user.is_authenticated or not isinstance(request.user, Paziente):
         return HttpResponseRedirect(reverse("login"))
-    return render(request, "user/paziente.html")
+    return render(request, "users/paziente.html")
 
 def segreteria_view(request):
     if not request.user.is_authenticated or not isinstance(request.user, Segreteria):
         return HttpResponseRedirect(reverse("login"))
-    return render(request, "user/segreteria.html")
+    return render(request, "users/segreteria.html")
