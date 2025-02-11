eeb6a2fb92f9b13ae97471b9aec4afec01a732b0
RebeccaBarichello <162330521+RebeccaBarichello@users.noreply.github.com>
2025-02-11 21:44:44 +0100

    cerco di farlo funzionare

--- CasaSalute/users/urls.py
+++ CasaSalute/users/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from . import views
-
+from .views import (
+    pagina_medico, modifica_medico, 
+    pagina_infermiere, modifica_infermiere, 
+    pagina_paziente, prenota_visita
+)
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("medico", views.medico_view, name="medico"),
-    path("infermiere", views.infermiere_view, name="infermiere"),
-    path("paziente", views.paziente_view, name="paziente"),
-    path("segreteria", views.segreteria_view, name="segreteria"),
+    path('medico/', pagina_medico, name='pagina_medico'),
+    path('medico/modifica/', modifica_medico, name='modifica_medico'),
+    path('infermiere/', pagina_infermiere, name='pagina_infermiere'),
+    path('infermiere/modifica/', modifica_infermiere, name='modifica_infermiere'),
+    path('paziente/', pagina_paziente, name='pagina_paziente'),
+    path('paziente/prenota/', prenota_visita, name='prenota_visita'),
 ]
