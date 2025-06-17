dd6b9ee17a201bead7dcba36f5bf4e34286e131f
taniya kaur <kaurtaniya97@gmail.com>
2025-06-17 20:59:48 +0200

    problems with server resolved

--- CasaSalute/users/urls.py
+++ CasaSalute/users/urls.py
@@ -3,18 +3,16 @@ from .views import (
     login_view, logout_view,
     pagina_medico, modifica_medico, 
     pagina_infermiere, modifica_infermiere, 
-    pagina_paziente, prenota_visita,
-    pagina_segreteria
+    pagina_paziente, prenota_visita
 )
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('medico/', pagina_medico, name='medico_dashboard'),
+    path('medico/', pagina_medico, name='pagina_medico'),
     path('medico/modifica/', modifica_medico, name='modifica_medico'),
-    path('infermiere/', pagina_infermiere, name='infermiere_dashboard'),
+    path('infermiere/', pagina_infermiere, name='pagina_infermiere'),
     path('infermiere/modifica/', modifica_infermiere, name='modifica_infermiere'),
-    path('paziente/', pagina_paziente, name='paziente_dashboard'),
+    path('paziente/', pagina_paziente, name='pagina_paziente'),
     path('paziente/prenota/', prenota_visita, name='prenota_visita'),
-    path('segreteria/', pagina_segreteria, name='segreteria_dashboard'),
 ]
