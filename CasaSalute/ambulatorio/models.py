4cd5849e26424b531606227846f2e9f1c9f97d1c
taniya kaur <kaurtaniya97@gmail.com>
2025-06-25 12:15:13 +0200

    modifiche ambulatori

--- CasaSalute/ambulatorio/models.py
+++ CasaSalute/ambulatorio/models.py
@@ -1,15 +1,34 @@
 from django.db import models
 
-# MODELLO AMBULATORIO
 class Ambulatorio(models.Model):
-    TIPO = [
+    AMBULATORIO_TIPO = [
+        ('visite', 'Ambulatorio Visite'),
+        ('prestazioni', 'Ambulatorio Prestazioni'),
+    ]
+    id = models.BigAutoField(primary_key=True)
+    tipo = models.CharField(max_length=20, choices=AMBULATORIO_TIPO)
+
+    def __str__(self):
+        return f"{self.get_tipo_display()} (ID: {self.id})"
+
+class AmbulatorioVisite(models.Model):
+    ambulatorio = models.OneToOneField(Ambulatorio, on_delete=models.CASCADE)
+    VISITE_TIPO = [
         ('adulti', 'Per adulti'),
         ('pediatrico', 'Pediatrico'),
+    ]
+    sotto_tipo = models.CharField(max_length=20, choices=VISITE_TIPO)
+
+    def __str__(self):
+        return f"{self.ambulatorio} - {self.get_sotto_tipo_display()}"
+
+class AmbulatorioPrestazioni(models.Model):
+    ambulatorio = models.OneToOneField(Ambulatorio, on_delete=models.CASCADE)
+    PRESTAZIONI_TIPO = [
         ('prelievi', 'Sala prelievi'),
         ('medicazioni', 'Sala medicazioni'),
     ]
-    id = models.BigAutoField(primary_key=True)
-    tipo = models.CharField(max_length=20, choices=TIPO)
+    sotto_tipo = models.CharField(max_length=20, choices=PRESTAZIONI_TIPO)
 
     def __str__(self):
-        return f"Ambulatorio {self.id} ({self.get_tipo_display()})"
+        return f"{self.ambulatorio} - {self.get_sotto_tipo_display()}"
