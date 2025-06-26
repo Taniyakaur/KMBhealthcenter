c70d798ceb5b30be2ce91624f527ca002d527a30
taniya kaur <kaurtaniya97@gmail.com>
2025-06-26 17:27:28 +0200

    added time slot func

--- CasaSalute/visita/forms.py
+++ CasaSalute/visita/forms.py
@@ -3,6 +3,8 @@ from .models import Visita
 from .models import PrenotazioneVisita
 from users.models import Medico, Paziente
 from ambulatorio.models import Ambulatorio
+from .utils import get_slots
+from datetime import timedelta
 
 class VisitaForm(forms.ModelForm):
     class Meta:
@@ -17,12 +19,25 @@ class PrenotazioneForm(forms.ModelForm):
         fields = ['paziente', 'medico', 'data', 'ora', 'tipo', 'regime']
         widgets = {
             'data': forms.DateInput(attrs={'type': 'date'}),
-            'ora': forms.TimeInput(attrs={'type': 'time'}),
+            'ora': forms.Select(),  # Cambia in Select per mostrare solo gli slot disponibili
             'paziente': forms.Select(attrs={'class': 'form-control'}),
             'medico': forms.Select(attrs={'class': 'form-control'}),
             'tipo': forms.Select(attrs={'class': 'form-control'}),
             'regime': forms.Select(attrs={'class': 'form-control'})
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        data = self.initial.get('data') or self.data.get('data')
+        medico = self.initial.get('medico') or self.data.get('medico')
+        if data and medico:
+            from .models import PrenotazioneVisita
+            prenotazioni = PrenotazioneVisita.objects.filter(data=data, medico=medico)
+            slot_duration = timedelta(minutes=30)
+            slots = get_slots(data, slot_duration, prenotazioni)
+            self.fields['ora'].choices = [(s, s.strftime("%H:%M")) for s in slots]
+        else:
+            self.fields['ora'].choices = []
 
 # FORM PER INSERIMENTO ESITO VISITA
 class EsitoVisitaForm(forms.ModelForm):
