5feff7edbaf99cdd89b3ebfe29d5225619b792c8
fmagr <fede.magrograssi@gmail.com>
2025-06-27 16:52:46 +0200

    cose varie
    documentazione e email

--- CasaSalute/CasaSalute/settings.py
+++ CasaSalute/CasaSalute/settings.py
@@ -133,10 +133,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Email SMTP Settings per Gmail
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = 'tuoindirizzo@gmail.com'  # <-- metti la tua email
-#EMAIL_HOST_PASSWORD = 'password_app_gmail'  # <-- metti l'app password
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'fede.magrograssi@gmail.com'
+EMAIL_HOST_PASSWORD = 'byng cpsa yqfj fwnd'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 #DEFAULT_FROM_EMAIL = 'Casa della Salute <tuoindirizzo@gmail.com>'
